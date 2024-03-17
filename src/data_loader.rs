51e002c46dae0db5a44acc50ddfdb03300d6af84
Stefan Schade <stefan.schade@msg.group>
2024-03-17 21:29:16 +0100

    #10 adapt routine to load test data to reflect future domain changes

--- src/data_loader.rs
+++ src/data_loader.rs
@@ -1,33 +1,15 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-use std::sync::Arc;
-
-use serde::Deserialize;
-
 use crate::domain::aggregates::location::Location;
 use crate::domain::aggregates::passage::Passage;
 use crate::port::repository::{LocationRepository, PassageRepository};
+use serde_json;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::sync::Arc;
 
-#[derive(Deserialize)]
-struct LocationData {
-    id: i32,
-    title: String,
-    description: String,
-    image_url: Option<String>,
-}
-
-#[derive(Deserialize)]
-struct PassageData {
-    id: i32,
-    from_location_id: i32,
-    to_location_id: i32,
-    description: String,
-}
-
-pub fn load_data_from_json(
-    location_repo: Arc<dyn LocationRepository>,
-    passage_repo: Arc<dyn PassageRepository>,
+pub fn load_data_from_json<R: LocationRepository, P: PassageRepository>(
+    location_repo: Arc<R>,
+    passage_repo: Arc<P>,
     location_file_path: &Path,
     passage_file_path: &Path,
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -35,16 +17,10 @@ pub fn load_data_from_json(
     let mut file = File::open(location_file_path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let locations: Vec<LocationData> = serde_json::from_str(&contents)?;
+    let locations: Vec<Location> = serde_json::from_str(&contents)?;
 
     // Populate the location repository
-    for location_data in locations {
-        let location = Location {
-            id: location_data.id,
-            title: location_data.title,
-            description: location_data.description,
-            image_url: location_data.image_url,
-        };
+    for location in locations {
         location_repo.add_location(location)?;
     }
 
@@ -52,16 +28,10 @@ pub fn load_data_from_json(
     let mut file = File::open(passage_file_path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let passages: Vec<PassageData> = serde_json::from_str(&contents)?;
+    let passages: Vec<Passage> = serde_json::from_str(&contents)?;
 
     // Populate the passage repository
-    for passage_data in passages {
-        let passage = Passage {
-            id: passage_data.id,
-            from_location_id: passage_data.from_location_id,
-            to_location_id: passage_data.to_location_id,
-            description: passage_data.description,
-        };
+    for passage in passages {
         passage_repo.add_passage(passage)?;
     }
 
