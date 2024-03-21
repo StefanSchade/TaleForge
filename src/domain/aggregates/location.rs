a7edd143640418399a7ffe24bca4aa18d6e0d5ae
Stefan Schade <stefan.schade@msg.group>
2024-03-21 12:19:10 +0100

    #8 Unit testing the aggregate builders

--- src/domain/aggregates/location.rs
+++ src/domain/aggregates/location.rs
@@ -10,3 +10,23 @@ pub struct Location {
     #[builder(default)]
     pub image_url: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_location_builder() {
+        let location = LocationBuilder::default()
+            .id(1)
+            .title("Test Location Title".into())
+            .description("Test Location Description".into())
+            .build()
+            .unwrap();
+
+        assert_eq!(location.id, 1);
+        assert_eq!(location.title, "Test Location Title");
+        assert_eq!(location.description, "Test Location Description");
+        assert_eq!(location.image_url, None);
+    }
+}
--- src/domain/aggregates/passage.rs
+++ src/domain/aggregates/passage.rs
@@ -10,3 +10,27 @@ pub struct Passage {
     pub direction: String, // New field for navigation direction
     pub narration: String, // New field for descriptive narration
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::aggregates::passage::PassageBuilder;
+    use super::*;
+
+    #[test]
+    fn test_passage_builder() {
+        let passage = PassageBuilder::default()
+            .id(1)
+            .from_location_id(1)
+            .to_location_id(1)
+            .description("Description".into())
+            .direction("north".into())
+            .narration("Narration".into())
+            .build()
+            .unwrap();
+
+        assert_eq!(passage.id, 1);
+        assert_eq!(passage.description, "Description");
+        assert_eq!(passage.direction, "north");
+        assert_eq!(passage.narration, "Narration");
+    }
+}
