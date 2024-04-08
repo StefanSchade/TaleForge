01754b5764f42e3ccb4d5ea0c12ada5601b7705c
Stefan Schade <stefan.schade@msg.group>
2024-04-08 08:43:04 +0200

    #32 document various strategies for mapping

---
+++ app-backend/application/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod domain_story_impl;
+pub mod contract_implementations;
+pub mod dto_domain_mapping;
+
---
+++ app-backend/port/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod game_services;
+pub mod context;
+pub mod domain_stories;
+pub mod repositories;
+pub mod dto;
