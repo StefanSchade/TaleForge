924b4a0b6d2a6cb55a4c8778abb7c589750cd8bd
Stefan Schade <stefan.schade@msg.group>
2024-04-10 11:36:12 +0200

    #8 making server implementation testable

--- app-backend/adapter/src/web/server.rs
+++ app-backend/adapter/src/web/server.rs
@@ -8,17 +8,18 @@ use crate::web::controllers::player_controller;
 
 pub async fn start_server(app_state: Data<Arc<AppState>>) -> std::io::Result<()> {
     HttpServer::new(move || {
-        setup_app(app_state);
+        setup_app(app_state.clone())
     })
         .bind("localhost:8080")?
         .run()
         .await
 }
 
-pub fn setup_app(app_state: Data<Arc<AppState>>) -> App<T> {
+pub fn setup_app(app_state: Data<Arc<AppState>>) -> App {
     App::new()
         .app_data(app_state.clone())
         .service(
             web::resource("/player/move").route(web::post().to(player_controller::move_player)),
         )
+    // You can chain more configurations here as needed.
 }
