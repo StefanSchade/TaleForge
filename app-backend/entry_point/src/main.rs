5350063f91b59f41263540b18d7f1f89f825ead7
Stefan Schade <stefan.schade@msg.group>
2024-04-20 13:41:42 +0200

    #47 remove unused imports

--- app-backend/entry_point/src/main.rs
+++ app-backend/entry_point/src/main.rs
@@ -1,24 +1,19 @@
 use std::path::Path;
 use std::sync::Arc;
+
 use adapter::persistence::in_memory_repository::{InMemoryLocationRepository, InMemoryPassageRepository, InMemoryPlayerStateRepository};
 use adapter::web::adapter_01_actix::server;
-use application::contract_implementations::location_query_impl::LocationQueryImpl;
-use application::contract_implementations::passage_query_impl::PassageQueryImpl;
 use application::domain_story_impl::move_player_impl::MovePlayerDomainStoryImpl;
-use domain_contract::services::navigation_services::NavigationService;
-
 use port::dto::player_state_dto::PlayerStateDTO;
-
 use port::service_container::service_container::ServiceContainer;
 
 mod data_loader;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let location_repo = Arc::new(InMemoryLocationRepository::new());
-    let passage_repo =Arc::new(InMemoryPassageRepository::new());
-    let player_state_repo =Arc::new(InMemoryPlayerStateRepository::new());
+    let passage_repo = Arc::new(InMemoryPassageRepository::new());
+    let player_state_repo = Arc::new(InMemoryPlayerStateRepository::new());
 
     // let location_queries = Arc::new(LocationQueryImpl::new(location_repo));
     // let passage_queries =  Arc::new(PassageQueryImpl::new(passage_repo));
@@ -33,7 +28,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         location_repo,
         passage_repo,
         player_state_repo,
-        move_player_ds
+        move_player_ds,
     );
 
     // initialize the repos with data
