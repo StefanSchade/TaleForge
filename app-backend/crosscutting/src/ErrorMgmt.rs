cac5cb82bc30c37ebda6c796fa3673f20bcab741
Stefan Schade <stefan.schade@msg.group>
2024-04-21 15:27:59 +0200

    #52 improve error management

--- app-backend/crosscutting/src/ErrorMgmt.rs
+++ app-backend/crosscutting/src/ErrorMgmt.rs
@@ -1,17 +1,35 @@
-// In your new crosscutting crate:
+#[derive(Copy, Clone)]
 pub enum ErrorKind {
     Technical,
     Functional,
 }
 
+pub struct ErrorTemplate {
+    pub code: &'static str,
+    pub message: &'static str,
+    pub kind: ErrorKind,
+}
+
+impl ErrorTemplate {
+    pub fn instantiate(&self, parameters: Vec<String>) -> Error {
+        Error {
+            code: self.code.to_string(),
+            message: self.message.to_string(),
+            parameters,
+            kind: self.kind,
+        }
+    }
+}
+
 pub struct Error {
     pub code: String,
+    pub message: String,
     pub parameters: Vec<String>,
     pub kind: ErrorKind,
 }
 
-impl Error {
-    pub fn new(code: String, parameters: Vec<String>, kind: ErrorKind) -> Self {
-        Error { code, parameters, kind }
-    }
-}
+pub static DATABASE_CONNECTION_FAILURE: ErrorTemplate = ErrorTemplate {
+    code: "database_connection_failure",
+    message: "Failed to connect to the database.",
+    kind: ErrorKind::Technical,
+};
