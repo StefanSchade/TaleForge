7a74f07d45f7e31fd224986562ad0f8236fea285
Stefan Schade <stefan.schade@msg.group>
2024-04-29 08:21:37 +0200

    #56 introducte match.rs

---
+++ app-backend/domain_pure/src/model/match.rs
@@ -0,0 +1,21 @@
+use derive_builder::Builder;
+use serde::{Deserialize, Serialize};
+
+#[derive(Builder, PartialEq, Clone, Debug, Deserialize, Serialize)]
+pub struct Match {
+    #[builder(setter(into))]
+    aggregate_id: u64,
+    #[builder(setter(into))]
+    game_id: u64,
+    #[builder(setter(into))]
+    registered_participants: Vec<u64>,
+    #[builder(setter(into))]
+    status: MatchStatus,
+}
+
+#[derive(Builder, PartialEq, Clone, Debug, Deserialize, Serialize)]
+pub enum MatchStatus {
+    scheduled,
+    started,
+    finished,
+}
