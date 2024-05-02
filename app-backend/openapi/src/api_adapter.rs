19b693ed4bb2d27413fc53b025989a29628ac1f4
Stefan Schade <stefan.schade@msg.group>
2024-05-02 17:46:34 +0200

    #46 connect open api experiments 2

--- app-backend/openapi/src/api_adapter.rs
+++ app-backend/openapi/src/api_adapter.rs
@@ -11,17 +11,29 @@ pub trait BusinessAdapter {
     async fn move_player(&self, bout_id: u64, player_id: u64, direction: String) ->Result<String, String>;
 }
 
-struct ApiAdapter {
-    business_adapter: BusinessAdapter
+
+struct ApiAdapter<B: BusinessAdapter + Send + Sync> {
+    business_adapter: B,
 }
 
 #[async_trait]
-impl Api for ApiAdapter {
+impl<B: BusinessAdapter + Send + Sync> Api for ApiAdapter<B> {
     fn poll_ready(&self, _cx: &mut Context) -> Poll<Result<(), Box<dyn Error + Send + Sync + 'static>>> {
-        todo!()
+        Poll::Ready(Ok(()))
     }
 
-    async fn move_player_post(&self, move_player_post_request: MovePlayerPostRequest, context: &C) -> Result<MovePlayerPostResponse, ApiError> {
-        todo!()
+    async fn move_player_post(&self, move_player_post_request: MovePlayerPostRequest, _context: &C) -> Result<MovePlayerPostResponse, ApiError> {
+        let bout_id = move_player_post_request.bout_id;
+        let player_id = move_player_post_request.player_id;
+        let direction = move_player_post_request.direction;
+
+        match self.business_adapter.move_player(bout_id, player_id, direction).await {
+            Ok(result_json) => {
+                Ok(MovePlayerPostResponse::Status200(MovePlayerPost200Response { result: result_json }))
+            },
+            Err(error_message) => {
+                Ok(MovePlayerPostResponse::Status400(ErrorModel { message: error_message }))
+            }
+        }
     }
 }
